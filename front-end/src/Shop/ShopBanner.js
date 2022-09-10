feb4c016b1671590be5415650fd9cab39b93ee64
TonyDCoder5 <tonyaflex5@gmail.com>
2022-09-10 16:19:30 -0600

    finalize mobile layout

--- front-end/src/Shop/ShopBanner.js
+++ front-end/src/Shop/ShopBanner.js
@@ -15,7 +15,7 @@ export default function ShopBanner() {
           alt="GYS Shirt-Model-Blue"
         />
         <h3>See our upcoming designs!</h3>
-        <button className="btn">Shop Preview</button>
+        <a href="/shop"><button className="btn">Shop Preview</button></a>
       </div>
       <div>
       <img
@@ -23,7 +23,7 @@ export default function ShopBanner() {
           alt="GYS Shirt-Model-Blue"
         />
          <h3>100% Amercian made tees, find our more below</h3>
-        <button className="btn">Read More</button>
+         <a href="/shop"><button className="btn">Read More</button></a>
       </div>
       <div>
       <img
@@ -31,7 +31,7 @@ export default function ShopBanner() {
           alt="GYS Shirt-Model-Blue"
         />
          <h3>Buy them at our upcoming meetups & events!</h3>
-        <button className="btn">Events</button>
+         <a href="/events"><button className="btn">Events</button></a>
       </div>
       </div>
     </div>
